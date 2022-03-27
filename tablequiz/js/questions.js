247ed092b5f36c6e86fe69ab0391fca932d8c4c7
junkook <102426382+junkookBTS@users.noreply.github.com>
2022-03-27 14:44:24 +0530

    Add files via upload

---
+++ tablequiz/js/questions.js
@@ -0,0 +1,223 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "2 × 4",
+    answer: "8",
+    options: [
+      "2",
+      "8",
+      "12",
+      "6"
+    ]
+  },
+    {
+    numb: 2,
+    question: "3 × 7",
+    answer: "21",
+    options: [
+      "12",
+      "18",
+      "21",
+      "14"
+    ]
+  },
+    {
+    numb: 3,
+    question: "5 × 9",
+    answer: "45",
+    options: [
+      "50",
+      "45",
+      "70",
+      "30"
+    ]
+  },
+    {
+    numb: 4,
+    question: "3 × 3",
+    answer: "9",
+    options: [
+      "12",
+      "13",
+      "9",
+      "18"
+    ]
+  },
+    {
+    numb: 5,
+    question: "3 × 5",
+    answer: "15",
+    options: [
+      "20",
+      "25",
+      "15",
+      "10"
+    ]
+  },
+  {
+    numb: 6,
+    question: "7 × 8",
+    answer: "56",
+    options: [
+      "21",
+      "38",
+      "40",
+      "56"
+    ]
+  },
+  {
+    numb: 7,
+    question: "3 × 5",
+    answer: "15",
+    options: [
+      "20",
+      "25",
+      "15",
+      "10"
+    ]
+  },
+  {
+    numb: 8,
+    question: "6 × 5",
+    answer: "30",
+    options: [
+      "20",
+      "25",
+      "30",
+      "10"
+    ]
+  },
+  {
+    numb: 9,
+    question: "3 × 8",
+    answer: "24",
+    options: [
+      "40",
+      "25",
+      "24",
+      "18"
+    ]
+  },
+  {
+    numb: 10,
+    question: "9 × 8",
+    answer: "72",
+    options: [
+      "40",
+      "72",
+      "80",
+      "90"
+    ]
+  },
+  {
+    numb: 11,
+    question: "13 × 6",
+    answer: "78",
+    options: [
+      "78",
+      "52",
+      "65",
+      "72"
+    ]
+  },
+  {
+    numb: 12,
+    question: "15 × 3",
+    answer: "78",
+    options: [
+      "45",
+      "52",
+      "75",
+      "30"
+    ]
+  },
+  {
+    numb: 13,
+    question: "17 × 8",
+    answer: "136",
+    options: [
+      "80",
+      "120",
+      "126",
+      "136"
+    ]
+  },
+  {
+    numb: 14,
+    question: "12 × 6",
+    answer: "72",
+    options: [
+      "72",
+      "62",
+      "65",
+      "70"
+    ]
+  },
+  {
+    numb: 15,
+    question: "14 × 8",
+    answer: "112",
+    options: [
+      "128",
+      "112",
+      "130",
+      "138"
+    ]
+  },
+  {
+    numb: 16,
+    question: "18 × 9",
+    answer: "162",
+    options: [
+      "180",
+      "126",
+      "162",
+      "108"
+    ]
+  },
+  {
+    numb: 17,
+    question: "17 × 7",
+    answer: "119",
+    options: [
+      "102",
+      "121",
+      "96",
+      "119"
+    ]
+  },
+  {
+    numb: 18,
+    question: "19 × 5",
+    answer: "95",
+    options: [
+      "85",
+      "100",
+      "95",
+      "115"
+    ]
+  },
+  {
+    numb: 19,
+    question: "14 × 6",
+    answer: "84",
+    options: [
+      "90",
+      "87",
+      "92",
+      "94"
+    ]
+  },
+  {
+    numb: 20,
+    question: "12 × 7",
+    answer: "84",
+    options: [
+      "102",
+      "84",
+      "96",
+      "120"
+    ]
+  },
+];
